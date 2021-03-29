233ddf7eef9b8c7213baf421fba2f19fadbf6778
Ayan Sengupta <ayansengupta32@gmail.com>
2021-03-29 17:23:56 +0530

    Update BigBazaarScrapper.py
    Updated code!!

--- BigBazaarScrapper.py
+++ BigBazaarScrapper.py
@@ -3,28 +3,19 @@ import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path = r"C:\{"YOUR\PATH\HERE"}\chromedriver.exe")
-categories=["Fruits--Vegetables-279","Bakery--Dairy-283","Home-Fashion-1093","Kitchen-&-Dining-291","Meat-&-Non-Veg-1003","Grocery-&-Staples-281","Snacks-&-Packaged-Food-287","Spreads-&-Sauces-285"]
-
-for category in categories:
-    url = "https://shop.bigbazaar.com/catalog/category/"+category
-    driver.get(url)
-    data = {}
-    data['product'] = []
-    # Scroller
+def page_scroller(driver):
     last_height = driver.execute_script("return document.body.scrollHeight")
-    print(last_height)
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
         time.sleep(2)
         new_height = driver.execute_script("return document.body.scrollHeight")
-        print(new_height)
         if new_height == last_height:
             break
         last_height = new_height
-    # parser
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # products tag catcher
+
+def product_tag_catcher(soup):
+    data = {}
+    data['product'] = []
     products=soup.find_all(class_="col-xs-4 col-sm-4 similar")
     for product in products:
         # Product Name
@@ -49,8 +40,38 @@ for category in categories:
                 'Product Name': name,
                 'Member Price': member_price,
                 'Quantity': quantity})
+    return (data)
 
+def jsondump(data,soup):
     json_object = json.dumps(data, indent = 4)
-    with open(f"{category}.json", "w") as outfile:
+    with open(f"{soup.find('title').text}.json", "w") as outfile:
         outfile.write(json_object)
-driver.quit()
+
+
+if __name__=="__main__":
+    url_list=[]
+    try:
+        number_of_url=int(input("Enter the number of url's: "))
+        if(number_of_url>0):
+            print("Enter the URL/URL's you want to scrape")
+            for i in range(number_of_url):
+                url_list.append(input(f"url { i+1}:"))
+        elif(number_of_url<0):
+            print("Enter a positive number, Please!")
+            exit()
+
+    except ValueError:
+        print("You gotta enter something here buddy!!")
+        exit()
+    driver = webdriver.Chrome(executable_path = r"YOUR\PATH\HERE\chromedriver.exe")
+    for category in url_list:
+        driver.get(category)
+        page_scroller(driver)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        data=product_tag_catcher(soup)
+        jsondump(data,soup)
+    driver.quit()
+
+
+
+
